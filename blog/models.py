1b1c1bd0d858dd1fd913b34e3a94851deec77581
Lee HyunJoo <wayhome250@gmail.com>
2017-09-02 11:56:42 +0900

    blog앱 Post,Comment 모델 생성 및 마이그레이션

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
+from django_extensions.db.models import TimeStampedModel
+
 from django.db import models
 
-# Create your models here.
+
+class Post(TimeStampedModel):
+    title = models.CharField(max_length=180)
+    content = models.TextField()
+
+
+class Comment(TimeStampedModel):
+    post = models.ForeignKey('Post')
+    message = models.TextField()
