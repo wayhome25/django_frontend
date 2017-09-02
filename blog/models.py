65308aa84c058a73058aba27849eb38d9d9921b0
Lee HyunJoo <wayhome250@gmail.com>
2017-09-02 12:46:00 +0900

    blog.Post 모델에 대한 CRUD 구현

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,15 @@
 from django_extensions.db.models import TimeStampedModel
 
 from django.db import models
+from django.urls import reverse
 
 
 class Post(TimeStampedModel):
     title = models.CharField(max_length=180)
     content = models.TextField()
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.pk])
 
 class Comment(TimeStampedModel):
     post = models.ForeignKey('Post')
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
+
+from .models import Post
+
+index = ListView.as_view(model=Post, template_name='blog/index.html')
+
+post_new = CreateView.as_view(model=Post, fields='__all__')
+
+post_detail = DetailView.as_view(model=Post)
+
+post_edit = UpdateView.as_view(model=Post, fields='__all__')
 
 
-def index(request):
-    return render(request, 'blog/index.html')
+class PostDetailView(DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog:index')
+
+post_delete = PostDetailView.as_view()
